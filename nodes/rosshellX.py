8b2adfc1e460f3cd0200146403c4ebadf090b48d
andre-dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2015-09-14 10:27:01 +0200

    runs with catkin

--- nodes/rosshellX.py
+++ nodes/rosshellX.py
@@ -12,24 +12,24 @@ import time
 
 def callbackStdIn(msg):
 	global process
-	
+
 	# un/comment this to enable tostopic to publish strings...
 	msg.data = msg.data.replace("\\n", "\n")
-	
+
 	process.stdin.write(msg.data)
 	rospy.loginfo("stdin: " +msg.data)
 
 if __name__ == "__main__":
 
 	rospy.init_node('rosshellX')
-	
+
 	topicStdOut = rospy.get_param('~stdout', "/rosshellX/stdout")
 	topicStdErr = rospy.get_param('~stderr', "/rosshellX/stderr")
 	topicStdIn  = rospy.get_param('~stdin', "/rosshellX/stdin")
 	cmd_user    = rospy.get_param('~command', "")
 
 	#print len(cmd_user)
-	
+
 	# check if there is a command
 	if len(cmd_user) == 0:
 		if len(sys.argv) >= 2:
@@ -37,34 +37,34 @@ if __name__ == "__main__":
 		else:
 			rospy.logerr("no command to interprete defined, try _command:=\"command\"")
 			sys.exit()
-	
+
 	rospy.loginfo("execute command: " + cmd_user)
 	rospy.loginfo("stdout on: " + topicStdOut)
 	rospy.loginfo("stderr on:  " + topicStdErr)
-	rospy.loginfo("stdin on:  " + topicStdIn)	
-	
+	rospy.loginfo("stdin on:  " + topicStdIn)
+
 	# change the buffer mode of the subprocess-shell to linebufferd
-	cmd_stdbuf = "stdbuf -o L" 
+	cmd_stdbuf = "stdbuf -o L"
 	cmd = cmd_stdbuf + " " + cmd_user
 
 	process = sp.Popen(cmd.split(" "), stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE, bufsize=1)
 	rospy.loginfo("started process with pid " + str(process.pid))
 	rospy.loginfo("XXX")
 
-	pubOUT = rospy.Publisher( topicStdOut, String)
-	pubERR = rospy.Publisher( topicStdErr, String)
+	pubOUT = rospy.Publisher( topicStdOut, String, queue_size=10)
+	pubERR = rospy.Publisher( topicStdErr, String, queue_size=10)
 	subIN  = rospy.Subscriber(topicStdIn,  String, callbackStdIn)
-	
+
 	# putting readline of stdout and stderr into non-blocking mode
 	fd_out = process.stdout.fileno()
 	fl = fcntl.fcntl(fd_out, fcntl.F_GETFL)
 	fcntl.fcntl(fd_out, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-	
+
 	fd_err = process.stderr.fileno()
 	fl = fcntl.fcntl(fd_err, fcntl.F_GETFL)
 	fcntl.fcntl(fd_err, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-	
-	
+
+
 	while not rospy.is_shutdown():
 		try:
 			stdout = process.stdout.readline()
@@ -73,7 +73,7 @@ if __name__ == "__main__":
 				rospy.loginfo("stdout: " +stdout)
 		except Exception as e:
 			pass
-		
+
 		try:
 			stderr = process.stderr.readline()
 			if len(stderr) > 0:
@@ -81,13 +81,13 @@ if __name__ == "__main__":
 				rospy.logerr("stderr: " +stderr)
 		except Exception as e:
 			pass
-				
+
 		time.sleep(0.001)
-		
+
 		# process is terminated
-		if process.poll()!=0:
-			break
-	
+		#if process.poll()!=0:
+		#	break
+
 	#process.terminate()
 	ret = process.returncode
 	rospy.signal_shutdown('finish')
